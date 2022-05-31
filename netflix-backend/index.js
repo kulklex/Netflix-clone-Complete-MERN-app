dcaa8d1298e6e4f531f59aa0a7de0762823f56da
Kulklex <kulklex@gmail.com>
2022-05-31 09:41:08 +0100

    installed cryptojs

--- netflix-backend/index.js
+++ netflix-backend/index.js
@@ -1,28 +1,32 @@
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
-const dotenv = require("dotenv")
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const bodyParser = require("body-parser")
 
-dotenv.config()
+dotenv.config();
 
+const auth = require("./routes/auth")
+const users = require("./routes/users")
 
-mongoose.connect(process.env.MONGO_URL, {
+app.use(bodyParser.json())
+app.use(express.json())
+app.use("/auth", auth)
+app.use("/users", users)
+
+const port = 7000;
+mongoose
+  .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-})
-.then(
-    () => {
-        console.log("Connected to MongoDB")
-    }
-)
-.catch(
-    (err) => {
-        console.error(err)
-    }
-)
+  })
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Backend server is running on port ${port}`);
+    });
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
-
-const port = 8080
-app.listen(port, () => {
-    console(`Backend serveer is running on port ${port}`)
-})
